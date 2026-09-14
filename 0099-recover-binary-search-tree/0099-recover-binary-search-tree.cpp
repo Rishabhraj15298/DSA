d01ad81a9ceaffc9dbf3fd19952788b82471c98d
Rishabh Raj <161751526+Rishabhraj15298@users.noreply.github.com>
2026-09-14 18:28:46 +0530

    Time: 0 ms (100%), Space: 60.8 MB (78.56%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    TreeNode* prev = NULL;
+    TreeNode* first = NULL;
+    TreeNode* second = NULL;
+
+    void solve(TreeNode* root) {
+
+        if(root == NULL) {
+            return;
+        }
+
+        solve(root->left);
+
+        if(prev != NULL && root->val < prev->val) {
+
+            if(first == NULL) {
+                first = prev;
+            }
+
+            second = root;
+        }
+
+        prev = root;
+
+        solve(root->right);
+    }
+
+    void recoverTree(TreeNode* root) {
+
+        solve(root);
+
+        swap(first->val, second->val);
+    }
+};
