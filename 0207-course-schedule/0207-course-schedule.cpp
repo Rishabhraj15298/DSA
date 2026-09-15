8d4063946d5f950f9d4ccc4c5e9597750c8e9118
Rishabh Raj <161751526+Rishabhraj15298@users.noreply.github.com>
2026-09-15 18:13:59 +0530

    Time: 7 ms (46.43%), Space: 20.1 MB (34.7%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool TopologicalSort(unordered_map<int , vector<int>>&adj , int n , vector<int>&indegree){
+
+        queue<int>q;
+        int cnt =0;
+
+        for(int i =0;i<n;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+    
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            cnt++;
+            for(int &v : adj[u]){
+                 indegree[v] --;
+                 if(indegree[v] ==0){
+                    
+                    q.push(v);
+                }
+            }
+           
+        }
+        if(cnt == n){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+       
+
+        unordered_map<int , vector<int >>adj;
+        vector<int>indegree(numCourses , 0);
+
+        for(auto & i : prerequisites){
+            int u = i[1];
+            int v = i[0];
+
+            adj[u].push_back(v);
+            indegree[v]++;
+
+        }
+
+        return TopologicalSort(adj , numCourses , indegree);
+    }
+};
