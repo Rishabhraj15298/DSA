cc7185ff12479b50f892f669791e42a4dc97b166
Rishabh Raj <161751526+Rishabhraj15298@users.noreply.github.com>
2026-09-19 02:51:34 +0530

    Time: 3 ms (43.94%), Space: 8.5 MB (91.88%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x==0){
+            return true;
+
+        }
+        else if (x<0){
+
+            return false;
+        }
+        int dup=x;
+        int rev=0;
+        while(x!=0){
+            int digit=x%10;
+            if( (rev> INT_MAX/10) || (rev < INT_MIN/10) ){
+                return false;
+            }
+
+            rev=(rev*10)+digit;
+            x=x/10;
+        }
+        if(dup==rev)return true;
+        else return false;        
+        
+    }
+};
