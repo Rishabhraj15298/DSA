a727bb3722adbdc1b5af397aa65be80191e44ebf
Rishabh Raj <161751526+Rishabhraj15298@users.noreply.github.com>
2026-09-25 00:13:26 +0530

    Time: 26 ms (67.69%), Space: 27.4 MB (72.5%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,30 +1,64 @@
 class Solution {
 public:
-    int m , n ;
-    int t[1001][1001];
-    int solve(string &s1 , string &s2 , int i, int j ){
-        if(i >= s1.length() || j>=s2.length()){
-            return 0;
+
+    // ___________________________RECURSION + MEMOIZATION______________________-
+    // int m , n ;
+    // int t[1001][1001];
+    // int solve(string &s1 , string &s2 , int i, int j ){
+    //     if(i >= s1.length() || j>=s2.length()){
+    //         return 0;
+    //     }
+    //     if(t[i][j] != -1){
+    //         return t[i][j];
+    //     }
+
+
+    //     if(s1[i] == s2[j]){
+    //          return t[i][j] = 1 + solve(s1 , s2 , i+1 , j+1);
+    //     }
+    //     else{
+    //          return t[i][j] =   max(solve(s1 , s2 , i+1 , j) , solve(s1 , s2 , i , j+1));
+    //     }
+    // }
+
+    // int longestCommonSubsequence(string s1, string s2) {
+    //     m= s1.length();
+    //     n = s2.length();
+
+    //     memset(t , -1 , sizeof(t));
+
+    //    return  solve(s1 , s2 , 0 ,0 );
+    // }
+
+
+    // ______________________BOTTOM - UP APPROACH_________________________________
+
+    int longestCommonSubsequence(string s1 , string s2){
+        int m = s1.size();
+        int n = s2.size();
+
+        vector<vector<int>>t(m+1, vector<int>(n+1));
+
+        for(int r = 0;r<=m;r++){
+            t[r][0] = 0;
         }
-        if(t[i][j] != -1){
-            return t[i][j];
+        for(int c = 0;c<=n;c++){
+            t[0][c] = 0;
         }
 
-
-        if(s1[i] == s2[j]){
-             return t[i][j] = 1 + solve(s1 , s2 , i+1 , j+1);
+        for(int i = 1; i<=m;i++){
+            for(int j =1 ;j<=n;j++){
+                if(s1[i-1]  == s2[j-1]) {
+                    t[i][j] = 1+ t[i-1][j-1];
+                }
+                else{
+                    t[i][j] = max(t[i-1][j] , t[i][j-1]);
+                }
+            }
         }
-        else{
-             return t[i][j] =   max(solve(s1 , s2 , i+1 , j) , solve(s1 , s2 , i , j+1));
-        }
-    }
 
-    int longestCommonSubsequence(string s1, string s2) {
-        m= s1.length();
-        n = s2.length();
-
-        memset(t , -1 , sizeof(t));
-
-       return  solve(s1 , s2 , 0 ,0 );
+        return t[m][n];
+    
+    
     }
 };
