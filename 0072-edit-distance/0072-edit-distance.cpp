2adf7ee54dba50fdfb093b8f900c3ca0df41e371
Rishabh Raj <161751526+Rishabhraj15298@users.noreply.github.com>
2026-09-26 00:24:26 +0530

    Time: 11 ms (29.15%), Space: 13.2 MB (61.06%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,35 +1,52 @@
 class Solution {
 public:
-    int m , n ;
-    int t[502][502];
-    int solve(string &s1 , string &s2 , int i , int j ){
-        if(i == m){
-            // then we need to insert the remaining elements of s2
-            return n-j;
-        }
-        if(j==n){
-            //In this case , we need to remove the extra elements from the s1
-            return m-i ;
-        }
-        if(t[i][j] != -1){
-            return t[i][j];
-        }
+    // int m , n ;
+    // int t[502][502];
+    // int solve(string &s1 , string &s2 , int i , int j ){
+    //     if(i == 0 || j == 0){
+    //         return i+j;
+    //     }
+    //     if(t[i][j] != -1){
+    //         return t[i][j];
+    //     }
 
-        if(s1[i] == s2[j]){
-            return t[i][j] = solve(s1 , s2 , i+1 , j+1);
+    //     if(s1[i-1] == s2[j-1]){
+    //         return t[i][j] = solve(s1 , s2 , i-1 , j-1);
 
+    //     }
+    //     int insertC  =  1 + solve(s1 , s2 , i , j-1);
+    //     int removeC = 1 + solve(s1 , s2 , i-1 , j);
+    //     int replaceC = 1 + solve(s1 , s2 , i-1 , j-1);
+
+    //     return t[i][j] = min({insertC , removeC , replaceC});
+    // }
+
+    // int minDistance(string s1, string s2) {
+    //     m = s1.size();
+    //     n = s2.size();
+    //     memset(t , -1 , sizeof(t));
+    //     return solve(s1 , s2 , m , n);
+    // }
+
+    int minDistance(string s1 , string s2){
+        int m = s1.size();
+        int n = s2.size();
+
+        vector<vector<int>>t(m+1 , vector<int>(n+1));
+
+        for(int i =0;i<=m;i++){
+            for(int j = 0;j<=n;j++){
+                if(i == 0 || j == 0 ){
+                    t[i][j] = i+j;
+                }
+                else if(s1[i-1] == s2[j-1]){
+                    t[i][j] = t[i-1][j-1];
+                }
+                else{
+                    t[i][j] = 1 + min({t[i][j-1] , t[i-1][j] , t[i-1][j-1]});
+                }
+            }
         }
-        int insertC  =  1 + solve(s1 , s2 , i , j+1);
-        int removeC = 1 + solve(s1 , s2 , i+1 , j);
-        int replaceC = 1 + solve(s1 , s2 , i+1 , j+1);
-
-        return t[i][j] = min({insertC , removeC , replaceC});
-    }
-
-    int minDistance(string s1, string s2) {
-        m = s1.size();
-        n = s2.size();
-        memset(t , -1 , sizeof(t));
-        return solve(s1 , s2 , 0 , 0);
+        return t[m][n];
     }
 };
